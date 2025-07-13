47240cb0dfe3e92d64b032da3353e8c377e9a18f
Brandon O'Donoghue <brandon@brandch.com>
2025-07-13 08:41:15 -0600

    API is hooked up and displaying the weather

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/colorado/2025-07-13?key=X9CNXWSHWUESEV4FDA8ZNL5WL", {
-    mode: 'cors'
-})
+fetchData();
 
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+function fetchData() {
+    const cityTemp = document.getElementById('city-temp');
+    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/colorado/2025-07-13?key=X9CNXWSHWUESEV4FDA8ZNL5WL", {
+        mode: 'cors'
+    })
+    
+    .then(response => response.json())
+   // .then(data => console.log(data)) ** this is how you see the api data in log
+    .then(data => {
+        const temp = data.currentConditions.temp;
+        const conditions = data.currentConditions.conditions;
+        const humidity = data.currentConditions.humidity;
+        const windSpeed = data.currentConditions.windspeed;
 
+        const cityTemp = document.getElementById("city-temp");
+        const cityConditions = document.getElementById("weather");
+        const cityHumidity = document.getElementById("humidity");
+        const cityWindSpeed = document.getElementById("wind");
+
+        cityTemp.innerText = temp;
+        cityConditions.innerText = conditions;
+        cityHumidity.innerText = humidity;
+        cityWindSpeed.innerText = windSpeed;
+    })
+    .catch(error => console.error(error));
+}
